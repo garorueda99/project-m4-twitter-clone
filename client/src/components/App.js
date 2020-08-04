945dfdf0235698df2169e81d26ab0c722d4fad76
Andres Rueda <garo99@gmail.com>
2020-08-04 15:17:19 -0400

    router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,30 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
+import GlobalStyles from "./GlobalStyles";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello World!!</p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Wrapper>
+        <Header />
+        <Main>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/items/:itemId">
+              <ItemDetails />
+            </Route>
+          </Switch>
+        </Main>
+      </Wrapper>
+
+      <GlobalStyles />
+    </BrowserRouter>
   );
 }
 
