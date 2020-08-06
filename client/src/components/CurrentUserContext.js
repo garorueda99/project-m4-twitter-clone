7f19bf6c9b0e430dce6dc76b9c19c6a330d2af76
Andres Rueda <garo99@gmail.com>
2020-08-05 21:42:58 -0400

    fetch

---
+++ client/src/components/CurrentUserContext.js
@@ -0,0 +1,31 @@
+import React, { createContext, useEffect, useState } from 'react';
+
+export const CurrentUserContext = createContext(null);
+
+export const CurrentUserProvider = ({ children }) => {
+  useEffect(() => {
+    fetchItem();
+  }, []);
+
+  const [currentUser, setCurrentUser] = useState({});
+
+  const fetchItem = async () => {
+    try {
+      const data = await fetch('api/me/profile');
+      const userInfo = await data.json();
+      setCurrentUser(userInfo);
+    } catch (err) {
+      //Error handling here!
+    }
+  };
+
+  return (
+    <CurrentUserContext.Provider
+      value={{
+        currentUser,
+      }}
+    >
+      {children}
+    </CurrentUserContext.Provider>
+  );
+};
