49011d374e1d532c763fc7228873d8246e9e4b26
Andres Rueda <garo99@gmail.com>
2020-08-10 14:18:21 -0400

    meow buttom

--- client/src/components/TweetDoc.js
+++ client/src/components/TweetDoc.js
@@ -1,32 +1,46 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { COLORS } from '../theme';
 import { CurrentUserContext } from './CurrentUserContext';
 
 const TweetDoc = () => {
-  useEffect(() => {}, []);
-  const [numOfLetters, setNumOfLettres] = useState(0);
+  const [numOfLetters, setNumOfLetters] = useState(280);
   const { currentUser } = useContext(CurrentUserContext);
+  const meowButtonRef = useRef();
+  useEffect(() => {
+    if (numOfLetters <= 0) {
+      meowButtonRef.current.disabled = true;
+      meowButtonRef.current.style.background = 'green';
+    } else {
+      meowButtonRef.current.disabled = false;
+      meowButtonRef.current.style.background = COLORS.primary;
+    }
+  }, [numOfLetters]);
   return (
     <DocWrapper>
       <WritingField>
         <Avatar src={currentUser.profile.avatarSrc} />
         <InputText
           name={'TweetText'}
-          placeholder={'What is new?'}
+          placeholder={'What is happening?'}
           onChange={(e) => {
-            setNumOfLettres(e.target.value.length);
+            setNumOfLetters(280 - e.target.value.length);
           }}
         ></InputText>
       </WritingField>
-      <div>{numOfLetters}</div>
+      <InfoBar>
+        <MeowCount>{numOfLetters}</MeowCount>
+        <MeowButton ref={meowButtonRef} onClick={() => console.log('click')}>
+          Meow
+        </MeowButton>
+      </InfoBar>
     </DocWrapper>
   );
 };
 
 const DocWrapper = styled.div`
   border: 1px solid ${COLORS.line};
-  height: 250px;
+  height: 280px;
   padding: 10px;
 `;
 
@@ -41,12 +55,41 @@ const Avatar = styled.img`
   border-radius: 50%;
 `;
 
+const InfoBar = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: right;
+`;
+
 const InputText = styled.textarea`
   margin-left: 5px;
   flex: 1;
   border: none;
   outline: none;
   text-align: top;
+`;
+
+const MeowButton = styled.button`
+  font-size: 0.9rem;
+  display: flex;
+  justify-content: center;
+  padding: 12px 0;
+  appearance: none;
+  background: ${COLORS.primary};
+  border: none;
+  color: white;
+  border-radius: 2em;
+  overflow: hidden;
+  user-select: none;
+  cursor: pointer;
+  z-index: 1;
+  box-shadow: 0 0 1em rgba(white, 0.2);
+  outline: none;
+  width: 100px;
+`;
+
+const MeowCount = styled.div`
+  margin-right: 15px;
 `;
 
 export default TweetDoc;
